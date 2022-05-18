f1ccdbc046668524aabd185a493bcead9768084e
Kovalev Danil <opana.loog@gmail.com>
2022-05-18 11:04:38 +0300

    add kafka dependency

--- src/main/java/ua/tunepoint/shelf/Starter.java
+++ src/main/java/ua/tunepoint/shelf/Starter.java
@@ -2,12 +2,13 @@ package ua.tunepoint.shelf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableEurekaClient
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(exclude = {KafkaAutoConfiguration.class})
 public class Starter {
 
     public static void main(String[] args) {
