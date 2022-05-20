737ba8b309c5c0ffb405232beb550fd9d77fc144
Kovalev Danil <opana.loog@gmail.com>
2022-05-20 09:01:26 +0300

    enable scheduling

--- src/main/java/ua/tunepoint/shelf/Starter.java
+++ src/main/java/ua/tunepoint/shelf/Starter.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableEurekaClient
-//@EnableScheduling
+@EnableScheduling
 @SpringBootApplication(exclude = {KafkaAutoConfiguration.class})
 public class Starter {
 
